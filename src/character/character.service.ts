f97c97fd6e36af1da1f718edcec28095b5f678dc
duvanflor <duvan.flor@netto.com.co>
2024-01-20 19:35:07 -0500

    Fix: Character service response when successfull ok:1

--- src/character/character.service.ts
+++ src/character/character.service.ts
@@ -1,37 +1,47 @@
 import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { HttpService } from '@nestjs/axios';
-import { catchError, firstValueFrom } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 
-// import { Character } from './entities';
-import { CharacterResponse } from './interfaces';
-import { AxiosError } from 'axios';
+import { Character } from './entities';
+import {
+  CharacterResponse,
+  Character as CharacterInterface,
+} from './interfaces';
 
 @Injectable()
 export class CharacterService {
+  private readonly baseUrl = 'https://rickandmortyapi.com/api/character';
+
   /**
    * Here, we have used data mapper approch for this tutorial that is why we
    * injecting repository here. Another approch can be Active records.
    */
   constructor(
-    // @InjectRepository(Character)
-    // private readonly characterRepository: Repository<Character>,
+    @InjectRepository(Character)
+    private readonly characterRepository: Repository<Character>,
     private readonly httpService: HttpService,
   ) {}
 
-  async findAll(): Promise<CharacterResponse> {
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get<CharacterResponse>('https://rickandmortyapi.com/api/character')
-        .pipe(
-          catchError((error: AxiosError) => {
-            throw `An error happened. Msg: ${JSON.stringify(
-              error?.response?.data,
-            )}`;
-          }),
-        ),
-    );
-    return data;
+  async create(): Promise<{ ok: number }> {
+    let allCharacters: CharacterInterface[] = [];
+    let nextPage = `${this.baseUrl}?page=1`;
+
+    while (nextPage) {
+      const { data } = await firstValueFrom(
+        this.httpService.get<CharacterResponse>(nextPage),
+      );
+      allCharacters = allCharacters.concat(data.results);
+      nextPage = data.info.next;
+    }
+
+    for (const character of allCharacters) {
+      await this.characterRepository.save({
+        jsonbRickAndMorty: character,
+      });
+    }
+
+    return { ok: 1 };
   }
 }
